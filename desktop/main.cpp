9c0886ba7a4fe8c13f25499bea6b308dbc8e09ef
Thomas Carrill <thomas.carrill@gmail.com>
2026-01-13 10:38:40 -0500

    Make some optimizations

--- desktop/main.cpp
+++ desktop/main.cpp
@@ -5,9 +5,6 @@
 
 int main(void)
 {
-    int window_width = 400;
-    int window_height = 300;
-
     Render r = sdl_renderer(window_width, window_height); 
 
     // Simple SDL loop to keep window open
@@ -16,7 +13,6 @@ int main(void)
     Uint32 last_tick = 0;
 
     while (running) {
-        SDL_Event e;
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) {
                 running = 0;
@@ -35,7 +31,7 @@ int main(void)
         if (now - last_tick >= 1000) {
             last_tick = now;
             ClockTime t = get_current_time();
-            xclock_draw(&r, t, window_width, window_height);
+            xclock_draw(&r, t);
         }
         SDL_Delay(10);
     }
