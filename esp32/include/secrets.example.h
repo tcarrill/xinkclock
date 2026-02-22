3b624afc2e852b777fc0068e83bc45a902fd8350
Thomas Carrill <thomas.carrill@gmail.com>
2026-02-22 11:35:39 -0500

    Consolidate TZ_STRING

--- esp32/include/secrets.example.h
+++ esp32/include/secrets.example.h
@@ -5,12 +5,3 @@
 
 #define WIFI_SSID     "your-ssid"
 #define WIFI_PASSWORD "your-password"
-
-// POSIX TZ string for your timezone.
-// Examples:
-//   US Eastern:  "EST5EDT,M3.2.0/2,M11.1.0/2"
-//   US Central:  "CST6CDT,M3.2.0/2,M11.1.0/2"
-//   US Pacific:  "PST8PDT,M3.2.0/2,M11.1.0/2"
-//   UTC:         "UTC0"
-// Full list: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
-#define TZ_STRING "EST5EDT,M3.2.0/2,M11.1.0/2"
